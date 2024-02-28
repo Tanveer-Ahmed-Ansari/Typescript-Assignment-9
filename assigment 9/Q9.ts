8dfab259de2a1037734755b1e9fe149d13e5d64d
Tanveer-Ahmed-Ansari <159915506+Tanveer-Ahmed-Ansari@users.noreply.github.com>
2024-02-28 21:52:41 +0500

    Add files via upload

---
+++ assigment 9/Q9.ts
@@ -0,0 +1,6 @@
+// 9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+
+let num: number = 1794;
+console.log(`my favorite number, ${num}`)
